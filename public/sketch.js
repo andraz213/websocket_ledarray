bb106439b00c6a5ec9b99c517164deb07e6b7f95
Andraž Novak <anovak@plume.com>
2023-08-23 10:44:50 +0200

    hexit

--- public/sketch.js
+++ public/sketch.js
@@ -53,27 +53,40 @@ function drawPixels(){
   }
 }
 
+let maxDiff = 0;
 function emitPixels(){
-
+  let strmessage = "";
   let message = [];
   for(let i = 0; i<64; i++) {
     for (let j = 0; j < 32; j++) {
       let pixel = pixels[i][j];
-      if(pixel.r + pixel.g + pixel.b  >= 1) {
+      if(pixel.r + pixel.g + pixel.b  >= 0) {
 
         message.push({
           x: i,
           y: j,
           color: {r: int(pixels[i][j].r),g:int(pixels[i][j].g),b:int(pixels[i][j].b)}
         });
+
+        strmessage += hex(i*32 + j, 3)  + hex(int(pixels[i][j].r), 2) + hex(int(pixels[i][j].g), 2)+ hex(int(pixels[i][j].b), 2);
+
+
       }
 
 
     }
   }
   if(message.length > 0){
-    socket.emit('pixels', message);
-    //console.log(JSON.stringify(message).length);
+    socket.emit('pixels', strmessage);
+
+
+    let diff = (JSON.stringify(message).length - strmessage.length) / strmessage.length;
+
+    if(maxDiff < diff){
+      maxDiff = diff;
+    }
+    console.log("maxDiff", maxDiff);
+    //console.log(JSON.stringify(message).length  + " - " + strmessage.length + " = " + (JSON.stringify(message).length - strmessage.length));
   }
 
 }
@@ -81,7 +94,7 @@ function emitPixels(){
 
 
 function draw(){
-  console.log(frameRate());
+  //console.log(frameRate());
   checkMouse();
   drawPixels();
   if(frameCount % 2 == 0){
@@ -89,17 +102,28 @@ function draw(){
   }
 
   socket.on('pixels', (data) => {
+    let start = millis();
     //console.log("got data", data.length);
-    for(let i = 0; i<data.length; i++){
+    for(let i = 0; i<data.length; i+= 9){
 
-      let pixel = data[i];
-      //console.log("color", pixel.color);
+      let index = unhex(data[i] + data[i+1] + data[i+2]);
+      let x = floor(index/32);
+      let y = index % 32;
+      let r = unhex(data[i+3] + data[i+4]);
+      let g = unhex(data[i+5] + data[i+6]);
+      let b = unhex(data[i+7] + data[i+8]);
+      //console.log("color", pixel);
 
-        fill(pixel.color.r,pixel.color.g,pixel.color.b);
+        fill(r,g,b);
         noStroke();
-        ellipse(pixel.x * 10 + 5, pixel.y * 10 + 5, 8, 8);
+        ellipse(x * 10 + 5, y * 10 + 5, 8, 8);
       //console.log("drawing", pixel.x, pixel.y, pixel.color);
       }
+
+    let stop = millis();
+    console.log("time", stop - start);
+    console.log("data", data.length);
+    console.log("....................................................");
   });
 
 }
